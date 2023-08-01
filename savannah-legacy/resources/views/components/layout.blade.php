20ea9b8c6566227c76f35aa7528d563885b6d35d
jesus95santana <jesus95santana@yahoo.com>
2023-08-01 16:02:49 -0400

    install sasscript

--- savannah-legacy/resources/views/components/layout.blade.php
+++ savannah-legacy/resources/views/components/layout.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
         <!-- Styles -->
-        @vite('resources/css/app.css')
+        @vite(['resources/css/app.css','resources/scss/app.scss','resources/js/app.js'])
     </head>
     <x-header></x-header>
     <body class="antialiased">

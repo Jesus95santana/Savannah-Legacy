567f3a57b8321613c31ad656e42bb06541de455a
jesus95santana <jesus95santana@yahoo.com>
2023-08-02 14:35:31 -0400

    added foot content

--- savannah-legacy/resources/views/components/footer.blade.php
+++ savannah-legacy/resources/views/components/footer.blade.php
@@ -1,3 +1,11 @@
 <footer>
-    I am footer
+    <div class="footerContainer">
+        <figure class="savBanner"><img src="/img/savBanner.jpg" alt="Savannah Banner"></figure>
+        <section class="footTextBox">
+            <p></p>
+            <p></p>
+            <p></p>
+        </section>
+        <figure class="bottomFootImg"><img src="/img/footBanner" alt="Foot Banner"></figure>
+    </div>
 </footer>

f4f8a4ad59d0a05307aeef7fc0da454f3489acaa
jesus95santana <jesus95santana@yahoo.com>
2023-08-02 14:46:33 -0400

    Add Footer Styles

--- savannah-legacy/resources/views/components/footer.blade.php
+++ savannah-legacy/resources/views/components/footer.blade.php
@@ -1,11 +1,11 @@
 <footer>
-    <div class="footerContainer">
-        <figure class="savBanner"><img src="/img/savBanner.jpg" alt="Savannah Banner"></figure>
-        <section class="footTextBox">
-            <p></p>
-            <p></p>
-            <p></p>
-        </section>
-        <figure class="bottomFootImg"><img src="/img/footBanner" alt="Foot Banner"></figure>
+    <div class="footerContainer bg-[#2abab0]">
+        <div class="footBox items-center flex flex-col p-4 text-white">
+            <figure class="savBanner"><img src="/img/footLogo.jpg" alt="Savannah Banner"></figure>
+            <p class="">1010 E. 36th Street, Savannah, GA 31401 </p>
+            <p>MAIN OFFICE: (912) 651-9662 FAX: (912) 651-9695</p>
+            <p>EMAIL: INFO@SAVLEGACYACADEMY.COM</p>
+        </div>
+        <figure class="bottomFootImg"><img src="/img/footBanner.jpg" alt="Foot Banner"></figure>
     </div>
 </footer>

867ca5db94d87a5af0d5de2cf18a9dfa322bafab
jesus95santana <jesus95santana@yahoo.com>
2023-08-09 15:58:31 -0400

    Complete Content About Page

--- savannah-legacy/resources/views/about-the-academy.blade.php
+++ savannah-legacy/resources/views/about-the-academy.blade.php
@@ -1,25 +1,27 @@
 <x-layout>
-    <div class="aboutAcademyContainer">
+    <div class="aboutAcademyContainer flex max-w-[1200px] mx-auto my-10">
         <section class="left">
-            <h2>About The Savannah Legacy Academy</h2>
-            <article>
-                <figure><img src="/img/people/drBrown.jpg" alt=""></figure>
+            <h2 class="contentHeader">About The Savannah Legacy Academy</h2>
+            <article class="flex">
+                <figure class="min-w-[200px] mr-7">
+                    <img src="/img/people/drBrown.jpg" alt="">
+                </figure>
                 <div class="textBox">
                     <p>
                         For anyone seeking excellence in a Christ-centered education as an extension of the Christian home, the Savannah Legacy Academy is the place to be! Savannah Legacy is an independent, private Christian school that provides an accredited preparatory curriculum program for students in grades Kindergarten through 8th grade.
-
+                        <br><br>
                         We strive to place God and His infallible word at the center of subjects studied, instruction given, and policies followed. We seek to incorporate these Christian principles in daily classroom devotions and instruction, weekly chapels, personal attention in small classroom settings, and giving assistance to students with special needs.
-
+                        <br><br>
                         Our Board, Administration, and Faculty are unified in our vision to partner with parents to develop young men and women, who are growing in their walk with Christ and seeking ways to impact the world in which they live, is our vision.
-
+                        <br><br>
                         Thank you for considering the Savannah Legacy Academy for your child's educational needs. As helpful as we hope that our website it in describing the Savannah Legacy experience, there is no substitute for a personal visit. We invite you to come to our campus and see for yourself what a difference our school can make in the life of your child. We know you'll be glad that you did and we hope to meet you soon.
-
-                        Dr. Morris Brown
+                        <br><br>
+                        Dr. Morris Brown<br>
                         Headmaster
                     </p>
                 </div>
             </article>
         </section>
-        <section class="right"></section>
+        <section class="right border border-red-600 min-w-[290px]"></section>
     </div>
 </x-layout>

e915d1299bd63d5c3559ae926abe7255877d6f98
jesus95santana <jesus95santana@yahoo.com>
2023-08-22 16:25:57 -0400

    create admission SideNav

---
+++ savannah-legacy/resources/views/components/admissionSideNav.blade.php
@@ -0,0 +1,13 @@
+<aside>
+    <div class="sideContainer">
+        <nav class="sidePages">
+            <li><a href={{ route('admissions') }}>ADMISSIONS</a></li>
+            <li><a href="">ACCREDITATION</a></li>
+            <li><a href="">TUITION & FEES</a></li>
+            <li><a href="">FINANCIAL AID</a></li>
+            <li><a href="">TRANSPORTATION</a></li>
+            <li><a href="">FREQUENTLY ASKED QUESTIONS</a></li>
+            <li><a href="">APPLY NOW/ONLINE APPLICATION</a></li>
+        </nav>
+    </div>
+</aside>

acbe7fb0eb016217b25e1691e9746ecbd2412206
jesus95santana <jesus95santana@yahoo.com>
2023-08-28 16:26:57 -0400

    Complete transportation page

--- savannah-legacy/resources/views/transportation.blade.php
+++ savannah-legacy/resources/views/transportation.blade.php
@@ -1,1 +1,33 @@
-<x-layout></x-layout>
+<x-layout>
+    <div class="transportationContainer flex max-w-[1200px] mx-auto mt-4 mb-10 flex-wrap justify-center">
+        <section class="left max-w-2xl ">
+            <h2 class="contentHeader">Transportation</h2>
+            <article>
+                <div class="textBox">
+                    <p>
+                        The Savannah Legacy Academy does not currently offer transportation for students to and from the school.
+                        <br><br>
+                        However, many of our parents carpool their students, create walking groups, or utilize city buses or morning/after-care busing.
+                        <br><br>
+                        Some of the after care busing options are:
+                        <br><br>
+                    </p>
+                    <ol>
+                        <li>Urban Hope Busing Service 912-349-5475</li>
+                        <li>Happy Tots Learning Center 912-663-0985</li>
+                        <li>Childcare Network, 7360 Hodgson Memorial Drive 912-355-4754</li>
+                        <li>Childcare Network, 12441 White Bluff 912-925-2534</li>
+                        <li>Kat’s Little Angels Daycare 912-235-2534</li>
+                    </ol>
+                </div>
+
+
+            </article>
+        </section>
+
+        <section class="right min-w-[350px]">
+            <x-admissionSideNav></x-admissionSideNav>
+        </section>
+
+    </div>
+</x-layout>
